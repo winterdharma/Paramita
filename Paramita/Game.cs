d3873bb3c5a254979b958b10e3ea79eaaa764022
Charlie Patton <cdpatton2009@gmail.com>
2016-12-13 18:38:06 -0600

    Finished Tutorial 1 for RogueSharp

---
+++ Paramita/Game.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RogueSharp;
+using RogueSharp.MapCreation;
+using System;
+
+namespace Paramita
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private IMap _map;
+        private Texture2D _floor;
+        private Texture2D _wall;
+
+        public Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IMapCreationStrategy<Map> mapCreationStrategy =
+                new RandomRoomsMapCreationStrategy<Map>(50, 30, 100, 7,3);
+            _map = Map.Create(mapCreationStrategy);
+            Console.WriteLine(_map.ToString());
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _floor = Content.Load<Texture2D>("floor");
+            _wall = Content.Load<Texture2D>("wall");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+
+            int sizeOfSprites = 64;
+            float scale = 0.25f;
+
+            foreach(Cell c in _map.GetAllCells() )
+            {
+                if(c.IsWalkable)
+                {
+                    var position = new Vector2(c.X * sizeOfSprites * scale, c.Y * sizeOfSprites * scale);
+                    spriteBatch.Draw(_floor, position, null,null,null, 0.0f, new Vector2(scale,scale), Color.White);
+                }
+                else
+                {
+                    var position = new Vector2(c.X * sizeOfSprites * scale, c.Y * sizeOfSprites * scale);
+                    spriteBatch.Draw(_wall, position, null, null, null, 0.0f, new Vector2(scale, scale), Color.White);
+                }
+            }
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
