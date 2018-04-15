056ae192304629fa3c01f961216f998ba36cae8c
Charlie Patton <cdpatton2009@gmail.com>
2018-04-15 08:56:59 -0500

    Added a change.

--- Paramita.GameLogic/Utility/GenericEventArgsClasses.cs
+++ Paramita.GameLogic/Utility/GenericEventArgsClasses.cs
@@ -14,22 +14,4 @@ namespace Paramita.GameLogic.Utility
             Destination = tileDest;
         }
     }
-
-    public class PointEventArgs : EventArgs
-    {
-        public Point Point { get; }
-        public PointEventArgs(Point point)
-        {
-            Point = point;
-        }
-    }
-
-    public class RectangleEventArgs : EventArgs
-    {
-        public Rectangle Rectangle { get; }
-        public RectangleEventArgs(Rectangle rectangle)
-        {
-            Rectangle = rectangle;
-        }
-    }
 }
